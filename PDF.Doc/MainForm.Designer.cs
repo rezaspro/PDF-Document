36b5716a3de2f3d982befa854c875afcfad2fd86
Md. Shakil Reza <rezaspro@gmail.com>
2019-07-24 16:57:25 +0600

    Initial commit

---
+++ PDF.Doc/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PDF.Doc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGeneratePdf = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePdf
+            // 
+            this.btnGeneratePdf.Location = new System.Drawing.Point(59, 13);
+            this.btnGeneratePdf.Name = "btnGeneratePdf";
+            this.btnGeneratePdf.Size = new System.Drawing.Size(96, 23);
+            this.btnGeneratePdf.TabIndex = 0;
+            this.btnGeneratePdf.Text = "Generate PDF";
+            this.btnGeneratePdf.UseVisualStyleBackColor = true;
+            this.btnGeneratePdf.Click += new System.EventHandler(this.btnGeneratePdf_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Merge PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGeneratePdf);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGeneratePdf;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PDF.Doc/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF.Doc
+{
+    public partial class MainForm : Form
+    {
+        //https://www.mikesdotnetting.com/article/87/itextsharp-working-with-images
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeneratePdf_Click(object sender, EventArgs e)
+        {
+            using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+            {
+                Document document = new Document(PageSize.A4, 10, 10, 10, 10);
+
+                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                document.Open();
+                Paragraph para = new Paragraph("This is from paragraph. Page2");
+                //doc.Add(new Paragraph("GIF"));
+                string startupPath = Application.StartupPath;
+                string imagePath = Path.Combine(startupPath, "Resources\\md_5ae458c89759c.png");
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagePath);
+
+                
+                document.Add(para);
+                document.Add(new Paragraph("Image"));
+                document.Add(img);
+                document.Close();
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestDocMetro.pdf");
+                using (FileStream fs = File.Create(path))
+                {
+                    fs.Write(bytes, 0, (int)bytes.Length);
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //String[] sourceFiles = @"E:\Test.pdf,E:\Test2.pdf".Split(',');
+        }
+    }
+}
